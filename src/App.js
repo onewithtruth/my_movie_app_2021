ae7c21e182fa359c57fe4b7e14d5b459c796a60f
Hojin Jang <onewithtruth@gmail.com>
2021-10-06 23:53:55 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { HashRouter, Route } from "react-router-dom"
-import Home from "./routes/Home"
-import About from "./routes/About"
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <HashRouter>
-      <Route path="/" exact={true}>
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-    </HashRouter>
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
+    </BrowserRouter>
   )
 }
 
